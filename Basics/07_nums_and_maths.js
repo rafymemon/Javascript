ea0a6fb96ef9a31b1e0e66565ea15879b2915332
Rafy Mustafa <128308433+rafymemon@users.noreply.github.com>
2025-01-28 06:24:08 +0000

    Basics of numbers and their related methods

---
+++ Basics/07_nums_and_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const otherName = 123.8596
+console.log(otherName.toPrecision(4)); //roundup to the presiced value given
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')); //indicate the values by commas according to the US standard
+
+
+//++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++
+console.log(Math.PI); // 3.1415926....
+
+
+
