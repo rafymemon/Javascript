677bf138b6c742ed8eb83c99287f00a78d1b84b0
Rafy Mustafa <128308433+rafymemon@users.noreply.github.com>
2025-01-28 13:05:28 +0000

    Math and Numbers

--- Basics/07_nums_and_maths.js
+++ Basics/07_nums_and_maths.js
@@ -19,5 +19,18 @@ console.log(hundreds.toLocaleString('en-IN')); //indicate the values by commas a
 //++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++
 console.log(Math.PI); // 3.1415926....
 
+console.log(Math.abs(-4)) //change the negative value into positive
+console.log(Math.round(5.632)); //roundof the numbers to the nearest digits
+console.log(Math.ceil(5.32)); //round of to the upper digit
+console.log(Math.floor(5.32)); //round of to the lower digit
+console.log(Math.random()); //give a random value
+console.log(Math.sqrt(4)) //give the sqaure root of a value
+console.log(Math.min(3, 43, 53, 2,1, 88)); //Give the minimum value among the numbers
+console.log(Math.max(3, 43, 53, 2,1, 88)); //Give the maximum value among the numbers
 
+console.log((Math.random()*10) + 1);
 
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
