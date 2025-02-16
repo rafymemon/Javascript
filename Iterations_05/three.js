15a3a37c8e8062f9504c62cd9022fbf5cd3ba2de
Rafy Mustafa <128308433+rafymemon@users.noreply.github.com>
2025-02-16 11:36:42 +0500

    for-of, For-in, ForEach iterations

---
+++ Iterations_05/three.js
@@ -0,0 +1,35 @@
+
+// For-of loop
+
+const arr = [1, 2, 4, 5, 65, 33]
+
+for (const val of arr) {
+  //  console.log(val);
+    
+}
+
+
+const greetings = "Hello World!"
+
+for (const greet of greetings) {
+   // console.log(`Each Char is ${greet}`);
+    
+}
+
+
+// For-in loop
+
+const obj = {a: 1, b: 2, c: 3};
+
+for (const key in obj) {
+  //  console.log(`Key: ${key}, Value: ${obj[key]}`);
+  }
+
+
+// forEach loop
+
+const numbers = [10, 20, 30, 40];
+
+numbers.forEach((number, index) => {
+ // console.log(`Index: ${index}, Number: ${number}`);
+});
